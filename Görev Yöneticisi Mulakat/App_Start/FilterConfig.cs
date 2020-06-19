4d8fbef8842da850115e52806c0aa61001ca8fc4
uznzafer <uznzafer@hotmail.com>
2020-06-19 16:15:47 +0300

    Add project files.

---
+++ Görev Yöneticisi Mulakat/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Görev_Yöneticisi_Mulakat
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Görev Yöneticisi Mulakat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Görev_Yöneticisi_Mulakat.MvcApplication" Language="C#" %>
